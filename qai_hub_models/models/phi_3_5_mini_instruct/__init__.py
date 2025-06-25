df4c8217547f8a984e9012efffdb04f644be9299
tom1030507 <tom1030507@gmail.com>
2025-06-24 23:17:00 -0700

    feat: Add export functionality for phi_3_5_mini_instruct model
    - Add model.py with 8 precompiled components (4 PromptProcessor + 4 TokenGenerator parts)
    - Add __init__.py to export Model class
    - Add export.py script following precompiled LLM pattern
    - Fix ImportError when trying to import Model from phi_3_5_mini_instruct
    - Support w4a16 precision and multiple Snapdragon devices
    - Enable model export functionality previously missing from the module

---
+++ qai_hub_models/models/phi_3_5_mini_instruct/__init__.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------------------
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# ---------------------------------------------------------------------
+from qai_hub_models.models.phi_3_5_mini_instruct.model import MODEL_ID  # noqa: F401
+from qai_hub_models.models.phi_3_5_mini_instruct.model import (  # noqa: F401
+    Phi_3_5_Mini_Instruct as Model,
+) 
